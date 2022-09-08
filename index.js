0bc31041e6805a90619ba826f828f884b7be9cb2
Sasi Patterson <sweetpea_th@hotmail.com>
2022-09-08 16:48:59 -0400

    nothing changed

--- index.js
+++ index.js
@@ -1,67 +1,75 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
-const fs = require('fs'); 
+const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown');
-
-// TODO: Create an array of questions for user input
-inquirer.prompt([
-    {
-        message: 'What is the name for your project?',
-        name: 'title',
-    },
-    {
-        message: 'What is your project about?',
-        name: 'description',
-    },
-    {
-        message: 'How to install your application?',
-        name: 'installation',
-    },
-    {
-        message: 'How to use your application?',
-        name: 'usage',
-    },
-    {
-        message: 'Any contribution to your project?',
-        name: 'contribution',
-    },
-    {
-        message: 'How do you test your application?',
-        name: 'test',
-    },
-    {
-        message: 'GitHub username',
-        name: 'github',
-    },
-    {
-        message: 'Your e-mail address?',
-        name: 'email',
-    },
-    {   
-        type: 'list',
-        message: 'Choose your license',
-        name: 'license',
-        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
-    },
- ]).then((data) => console.log(data));
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, userInput) {
-    fs.writeFile(fileName, userInput),(err) => err ? console.error(err) : console.log('Your README is ready!');
-}
-    
-// TODO: Create a function to initialize app
-function init() {
-    writeToFile('README.md', generateMarkdown(data));    
-    // inquirer.prompt(questions)
-    // .then(function(data){
-    //     writeToFile("README.md", data);
-    // });
+    fs.writeFile(fileName, userInput, (err) => {
+        err ? console.error(err) : console.log('Your README is ready!');
+    });
 }
 
-// Function call to initialize app
+// TODO: Create an array of questions for user input
+const init = () => {
+    inquirer.prompt([
+        {   
+            type: 'input',
+            message: 'What is the name for your project?',
+            name: 'title',
+        },
+        {
+            type: 'input',
+            message: 'What is your project about?',
+            name: 'description',
+        },
+        {
+            type: 'input',
+            message: 'How to install your application?',
+            name: 'installation',
+        },
+        {
+            type: 'input',
+            message: 'How to use your application?',
+            name: 'usage',
+        },
+        {
+            type: 'input',
+            message: 'Any contribution to your project?',
+            name: 'contribution',
+        },
+        {
+            type: 'input',
+            message: 'How do you test your application?',
+            name: 'test',
+        },
+        {
+            type: 'input',
+            message: 'GitHub username',
+            name: 'github',
+        },
+        {
+            type: 'input',
+            message: 'Your e-mail address?',
+            name: 'email',
+        },
+        {
+            type: 'list',
+            message: 'Choose your license',
+            name: 'license',
+            choices: 
+            [
+                'MIT', 
+                'APACHE 2.0', 
+                'GPL 3.0', 
+                'BSD 3', 
+                'None'
+            ],
+        },
+    ]).then((data) => {
+        console.log(data);
+        writeToFile('output/README.md', generateMarkdown(data));
+    });
+};
+
 init();
-
-// fs.writeFile('log.txt', process.argv[2], (err) =>
-//   err ? console.error(err) : console.log('Success!')
-// );
